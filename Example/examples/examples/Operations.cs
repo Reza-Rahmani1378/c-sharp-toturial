27dc9e3e037aef520588c3b12268ce0967a887a3
Reza <reza6273757@gmail.com>
2022-12-24 02:14:19 +0330

    Add Arrays

--- Example/examples/examples/Operations.cs
+++ Example/examples/examples/Operations.cs
@@ -122,3 +122,19 @@ while (m < 10)
     Console.WriteLine(m);
     m++;
 }
+
+/*
+ * Arrays 
+ */
+
+// Create an array of four elements, and add values later
+string[] cars1 = new string[4];
+
+// Create an array of four elements and add values right away 
+string[] cars2 = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
+
+// Create an array of four elements without specifying the size 
+string[] cars3 = new string[] { "Volvo", "BMW", "Ford", "Mazda" };
+
+// Create an array of four elements, omitting the new keyword, and without specifying the size
+string[] cars4 = { "Volvo", "BMW", "Ford", "Mazda" };
