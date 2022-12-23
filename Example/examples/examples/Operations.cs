1dc58a487248fe411ba923f0a0d207c55fd28fcb
Reza <reza6273757@gmail.com>
2022-12-23 22:36:53 +0330

    Interpolation in String

--- Example/examples/examples/Operations.cs
+++ Example/examples/examples/Operations.cs
@@ -2,3 +2,14 @@
 int y = 6;
 
 Console.WriteLine(x > y);
+
+/*
+    Interpolation in String 
+ */
+
+string firstName = "Reza";
+string lastName = "Rahmani";
+
+string fullName = $"My full name is :{firstName} {lastName}";
+
+Console.WriteLine(fullName);
