35d88958d83bf4b9f0d6603aa17ae8a4acf9ca69
Reza <reza6273757@gmail.com>
2022-12-23 22:01:44 +0330

    Refactor The Code.

--- Example/examples/examples/Program.cs
+++ Example/examples/examples/Program.cs
@@ -4,3 +4,9 @@ Console.WriteLine("Enter Username :");
 string username = Console.ReadLine();
 
 Console.WriteLine("Username is :" + username);
+
+Console.WriteLine("Enter Your Age :");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your age is :" + age);
