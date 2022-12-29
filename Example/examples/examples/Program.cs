33ff18703d4308da1fc5701aa18542a8d5d97b57
Reza <reza6273757@gmail.com>
2022-12-29 15:06:34 +0330

    Use Operator typeOf for get Class Name

--- Example/examples/examples/Program.cs
+++ Example/examples/examples/Program.cs
@@ -34,6 +34,9 @@ namespace examples
             string outUsername = dict.GetValueOrDefault(1, username);
             Console.WriteLine(outUsername);
 
+            string nameClass = typeof(Program).Name;
+            Console.WriteLine(nameClass);
+
         }
     }
 }
